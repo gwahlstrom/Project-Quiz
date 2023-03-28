558dd647a68b93c06832c05df37459b08cf8321b
MonicaCodreanu <monica.codreanu@gmail.com>
2023-03-28 15:03:52 +0200

    results page and progress bar

--- ProjectQuiz/src/components/results.jsx
+++ ProjectQuiz/src/components/results.jsx
@@ -5,9 +5,39 @@ import { QuizContext } from "./context";
 
 function Results(props) {
   const { data, setGameStart } = useContext(QuizContext);
+
   useEffect(() => {
     setGameStart(false);
   }, []);
+
+  const results = data.map((item, index) => {
+    const indexCorrectAnswer = props.possibleAnswers[index].findIndex(
+      (el) => el === item.correctAnswer
+    );
+    return (
+      <div className="QandA" key={index}>
+        <h4>
+          {item.question} -{" "}
+          {props.userAnswers[index] === item.correctAnswer
+            ? "Correct!"
+            : "Wrong answer"}
+        </h4>
+
+        <div className="answers">
+          {props.possibleAnswers[index].map((answer, indexAnsw) => {
+            return (
+              <p
+                className={indexAnsw === indexCorrectAnswer ? "green" : "red"}
+                key={answer}
+              >
+                {answer}
+              </p>
+            );
+          })}
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div className="results-wrapper">
@@ -16,6 +46,7 @@ function Results(props) {
         You had {props.correctAnswer} correct out of {props.amountOfAnswers}!
       </h2>
       <p>(visa alla frågor och de korrekta/inkorrekta svaren)</p>
+      {results}
       <Link to="/">Play again</Link>
     </div>
   );
