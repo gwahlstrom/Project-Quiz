39dcc58d75f8ebe8f2f1c1bf7417538005f0e3ef
Alexander Lind <alexander_lind95@hotmail.com>
2023-03-29 16:48:33 +0200

    results

--- ProjectQuiz/src/components/results.jsx
+++ ProjectQuiz/src/components/results.jsx
@@ -1,10 +1,19 @@
 import { Link } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useRef } from "react";
 import "./results.css";
 import { QuizContext } from "./context";
+import Confetti from "react-confetti";
 
 function Results(props) {
   const { data, setGameStart, setTimer } = useContext(QuizContext);
+  const [height, setHeight] = useState(null);
+  const [width, setWidth] = useState(null);
+  const confetiRef = useRef(null);
+
+  useEffect(() => {
+    setHeight(confetiRef.current.clientHeight);
+    setWidth(confetiRef.current.clientWidth);
+  }, []);
 
   useEffect(() => {
     setGameStart(false);
@@ -16,12 +25,18 @@ function Results(props) {
     );
     return (
       <div className="resultsQuestion" key={index}>
-        <h4>
-          {index + 1} - {item.question} -{" "}
-          {props.userAnswers[index] === item.correctAnswer
-            ? "Correct!"
-            : "Wrong answer"}
-        </h4>
+        <div className="wrongOrRightDiv">
+          <h2>
+            {props.userAnswers[index] === item.correctAnswer
+              ? "Correct answer"
+              : "Wrong answer"}
+          </h2>
+        </div>
+        <div className="resultQuestionsDiv">
+          <h4>
+            Question {index + 1}: {item.question}
+          </h4>
+        </div>
 
         <div className="resultsAnswer">
           {props.possibleAnswers[index].map((answer, indexAnsw) => {
@@ -44,15 +59,20 @@ function Results(props) {
   });
 
   return (
-    <div className="results-wrapper">
-      <h1>Results</h1>
-      <h2>
-        You had {props.correctAnswer} correct out of {props.amountOfAnswers}!
-      </h2>
+    <div className="results-wrapper" ref={confetiRef}>
+      <Confetti numberOfPieces={150} width={width} height={height} />
+      <div className="resultsInfo">
+        <h1>Results</h1>
+        <h2>
+          You had {props.correctAnswer} correct out of {props.amountOfAnswers}!
+        </h2>
+      </div>
       <div className="resultsScrollBox">{results}</div>
-      <Link to="/" className="playAgainBtn">
-        Play again
-      </Link>
+      <div className="playAgainDiv">
+        <Link to="/" className="playAgainBtn">
+          Play again
+        </Link>
+      </div>
     </div>
   );
 }
