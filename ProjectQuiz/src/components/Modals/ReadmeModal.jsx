2e5a9d408d31d2dc3743b25479dba3fb29912448
Gustav Wahlström <gustav.wahlstrom@brightstraining.com>
2023-04-04 11:13:26 +0200

    adding small changes

--- ProjectQuiz/src/components/Modals/ReadmeModal.jsx
+++ ProjectQuiz/src/components/Modals/ReadmeModal.jsx
@@ -14,9 +14,16 @@ function ReadmeModal({ setShowModal }) {
       <div className="readme-modal">
         <h2>Read Me</h2>
         <h4>This site was made using the following techniques:</h4>
-        <h5>React, Vite</h5>
-        <h5>HTML, CSS</h5>
-        <h5>Deployed using Netlify</h5>
+        <ul>
+          <li>React</li>
+          <li>Vite</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>
+            <a href="https://the-trivia-api.com/">Link to API</a>
+          </li>
+        </ul>
+        <h5>Deployed using Vercel</h5>
         <button onClick={() => setShowModal(false)}>
           <img src="x.svg" alt="close button icon" />
         </button>
